734d4b0424fd50e58fa08f418824fd19d538b426
Darko-gg <ofcorey@outlook.com>
2026-04-22 20:45:18 -0500

    Replace ping with initial threats command

--- commands/threat_commands.py
+++ commands/threat_commands.py
@@ -2,6 +2,6 @@ from discord.ext import commands
 
 
 def setup_threat_commands(bot):
-    @bot.command(name="ping")
-    async def ping(ctx):
-       await ctx.send("CyberOps Bot is responding.")
+    @bot.command(name="threats")
+    async def threats(ctx):
+        await ctx.send("Threat alert module is conneted and ready.")
